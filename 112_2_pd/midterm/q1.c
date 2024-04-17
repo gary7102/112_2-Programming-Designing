a0005302c0fc8c31f6162d26a86ab1983da91acf
gary7102 <160698915+gary7102@users.noreply.github.com>
2024-04-17 12:29:11 +0800

    minterm practice

--- 112_2_pd/midterm/q1.c
+++ 112_2_pd/midterm/q1.c
@@ -150,3 +150,51 @@ int main(){
     }
 }
 */
+#include <stdio.h>
+
+int main(){
+    int m, n;
+    scanf("%d %d", &m, &n);
+    if(m==1){
+        for(int i=0; i<n; i++){
+            for(int j=n-i-1; j>0; j--) printf("#");
+            for(int j=i+1; j>1; j--) printf("%d", j);
+            for(int j=1; j<=i+1; j++) printf("%d", j);
+            for(int j=n-i-1; j>0; j--) printf("#");
+            printf("\n");
+        }
+        for(int i=0; i<n-1; i++){
+            for(int j=0; j<i+1; j++) printf("#");
+            for(int j=n-i-1; j>1; j--) printf("%d", j);
+            for(int j=1; j<=n-i-1; j++) printf("%d", j);
+            for(int j=0; j<i+1; j++) printf("#");
+            printf("\n");
+        }
+    }else if(m==2){
+        for(int i=0; i<n; i++){
+            for(int j=1; j<=i+1; j++) printf("%d", j);
+            for(int j=n-i-1; j>0; j--) printf("#");
+            printf("\n");
+        }
+        for(int i=0; i<n; i++){
+            for(int j=n-i; j>0; j--) printf("%d", j);
+            for(int j=0; j<i; j++) printf("#");
+            printf("\n");
+        }
+    }else if(m==3){
+        for(int i=0; i<n; i++){
+            for(int j=2*(n-i-1); j>0; j--) printf("#");
+            for(int j=1; j<=i; j++) printf("%d", j);
+            for(int j=i+1; j>0; j--) printf("%d", j);
+            printf("\n");
+        }
+    }else if(m==4){
+        for(int i=0; i<n; i++){
+            for(int j=n-i-1; j>0; j--) printf("#");
+            if(i%2==0) for(int j=n; j>0; j--) printf("%d", j);
+            else for(int j=1; j<=n; j++) printf("%d", j);
+            for(int j=0; j<i; j++) printf("#");
+            printf("\n");
+        }
+    }
+}
