7f33eabcfca4b6cbeeb3dbee4027cb43cc7b027e
Gary <bernie71022@gmail.com>
2024-06-18 15:48:48 +0800

    update

--- 112_2_pd/Final/q1_p.c
+++ 112_2_pd/Final/q1_p.c
@@ -4,45 +4,41 @@
 
 #define MAX_LEN 1000
 
-typedef struct{
+typedef struct {
     char *word;
     int count;
 } WordEntry;
 
 void insertQbeforeP(char *article[], char p[], char q[], int totalWords){
-    char *newArticle[MAX_LEN];
-    int newWords = 0;
+    // char *newArticle[MAX_LEN];
+    // int newWords = 0;
+    // for(int i=0; i<totalWords; i++){
+    //     if(strcmp(article[i], p) == 0) newArticle[newWords++] = q;
+    //     newArticle[newWords++] = article[i];
+    // }
+    // for(int i=0; i<newWords; i++){
+    //     printf("%s ", newArticle[i]);
+    // }
     for(int i=0; i<totalWords; i++){
-        if(strcmp(article[i], p) == 0) newArticle[newWords++] = q;
-        newArticle[newWords++] = article[i];
+        if(strcmp(article[i], p) == 0) printf("%s ", q);
+        printf("%s ", article[i]);
     }
-    for(int i=0; i<newWords; i++){
-        printf("%s ", newArticle[i]);
-    }
-    printf("\n");
 }
 
 void replacePwithQ(char *article[], char p[], char q[], int totalWords){
     for(int i=0; i<totalWords; i++){
-        if(strcmp(article[i], p) == 0){
-            printf("%s ", q);
-        }else{
-            printf("%s ", article[i]);
-        }
+        if(strcmp(article[i], p) == 0) printf("%s ", q);
+        else printf("%s ", article[i]);
     }
-    printf("\n");
 }
 
 void deleteP(char *article[], char p[], int totalWords){
     for(int i=0; i<totalWords; i++){
-        if(strcmp(article[i], p) != 0){
-            printf("%s ", article[i]);
-        }
+        if(strcmp(article[i], p) != 0) printf("%s ", article[i]);
     }
-    printf("\n");
 }
 
-void removeLowFrequency(char *article[], int n, int totalWords){
+void removeLowFrequency(char *article[], int totalWords, int n){
     WordEntry entries[MAX_LEN] = {0};
     int uniqueWords = 0;
 
@@ -50,34 +46,34 @@ void removeLowFrequency(char *article[], int n, int totalWords){
         int found = 0;
         for(int j=0; j<uniqueWords; j++){
             if(strcmp(article[i], entries[j].word) == 0){
-                entries[j].count++;
                 found = 1;
+                entries[j].count++;
                 break;
             }
         }
         if(!found){
-            entries[uniqueWords].word = article[i];
             entries[uniqueWords].count = 1;
+            entries[uniqueWords].word = article[i];
             uniqueWords++;
         }
     }
 
+
     for(int i=0; i<totalWords; i++){
         for(int j=0; j<uniqueWords; j++){
-            if(strcmp(article[i], entries[j].word) == 0 && entries[j].count >= 3){
-                printf("%s ", article[i]);
-                break;
+            if((strcmp(article[i], entries[j].word) == 0) && entries[j].count >= n){
+                        printf("%s ", article[i]);
+                        break;
             }
         }
     }
-    printf("\n");
 }
 
 int compare(const void *a, const void *b){
     WordEntry *word1 = (WordEntry *)a;
     WordEntry *word2 = (WordEntry *)b;
-    if(word1->count != word2->count) return word1->count-word2->count;  // 升序
-    else return strcmp(word1->word, word2->word);    // 字典升序
+    if(word1->count != word2->count) return word1->count - word2->count;
+    else return strcmp(word1->word, word2->word);
 }
 
 void outputFrequency(char *article[], int totalWords){
@@ -88,27 +84,29 @@ void outputFrequency(char *article[], int totalWords){
         int found = 0;
         for(int j=0; j<uniqueWords; j++){
             if(strcmp(article[i], entries[j].word) == 0){
-                entries[j].count++;
                 found = 1;
+                entries[j].count++;
                 break;
             }
         }
         if(!found){
-            entries[uniqueWords].word = article[i];
             entries[uniqueWords].count = 1;
+            entries[uniqueWords].word = article[i];
             uniqueWords++;
         }
     }
-    qsort(entries, uniqueWords, sizeof(WordEntry), compare);
 
-    int count = (uniqueWords<3)? uniqueWords : 3;
-    for(int i=0; i<count; i++){
+    qsort(entries, uniqueWords, sizeof(WordEntry), compare);
+    
+    int index = (uniqueWords < 3)? uniqueWords : 3;
+    for(int i=0; i<index; i++){
         printf("%s:%d\n", entries[i].word, entries[i].count);
     }
+
 }
 
-int main(){
-    char p[100], q[100], article[MAX_LEN];
+void main(){
+    char article[MAX_LEN], p[100], q[100];
     int c, n=0;
 
     fgets(article, sizeof(article), stdin);
@@ -117,8 +115,9 @@ int main(){
     scanf("%s", p);
     scanf("%s", q);
     scanf("%d", &c);
-    if(c==4) scanf("%d", &n);
+    if(c == 4) scanf("%d", &n);
 
+    // tokenize the article
     char *arr[MAX_LEN];
     int totalWords = 0;
     char *token = strtok(article, " ");
@@ -126,25 +125,18 @@ int main(){
         arr[totalWords++] = token;
         token = strtok(NULL, " ");
     }
+    
 
-
-    switch (c)
-    {
-    case 1:
+    if(c==1){
         insertQbeforeP(arr, p, q, totalWords);
-        break;
-    case 2:
+    }else if(c == 2){
         replacePwithQ(arr, p, q, totalWords);
-        break;
-    case 3:
+    }else if(c == 3){
         deleteP(arr, p, totalWords);
-        break;
-    case 4:
-        removeLowFrequency(arr, n, totalWords);
-    case 5:
+    }else if(c == 4){
+        removeLowFrequency(arr, totalWords, n);
+    }else if(c == 5){
         outputFrequency(arr, totalWords);
-        break;
-    default:
-        break;
     }
+
 }
